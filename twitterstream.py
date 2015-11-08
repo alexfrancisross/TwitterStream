1d3d78ec96b6b1adfaaa71d6a17a89a23c10b5b3
alexfrancisross <alexfrancisross@hotmail.com>
2015-11-08 21:45:30 +0000

    upadte

--- twitterstream.py
+++ twitterstream.py
@@ -7,15 +7,15 @@ import psycopg2
 
 #connect to database
 try:
-    conn = psycopg2.connect("dbname='Tableau' user='Administrator' host='tableau.cuztkpb4hq8r.us-west-1.rds.amazonaws.com' password='password1'")
+    conn = psycopg2.connect("dbname='***DBNAME***' user='***USERNAME***' host='***HOSTNAME***' password='***PASSWORD***'")
 except:
     print "I am unable to connect to the database"
 
 #consumer key, consumer secret, access token, access secret.
-ckey="Xar3pvPceBcepR0sSEXwHv5Xf"
-csecret="tR9SJfPQDJdgiL2llm6LJi00hDPzNoPtkDgqPfIXbx4VWJjTIP"
-atoken="2893911603-K7qzcGrJOF4Yv5M24cky6eOfVP9h3dEm1feOaTy"
-asecret="Toznia43yjPRaU3PLyg0lUbV4gOfUcCZ0IzjowvOklGLj"
+ckey="***CKEY***"
+csecret="***CSECRET***"
+atoken="***ATOKEN***"
+asecret="***ASECRET***"
 
 #lookup arrays to convert user's timezone to a country
 TimeZones = ['International Date Line West','Midway Island','American Samoa','Hawaii','Alaska','Pacific Time (US & Canada)','Tijuana','Mountain Time (US & Canada)','Arizona','Chihuahua','Mazatlan','Central Time (US & Canada)','Saskatchewan','Guadalajara','Mexico City','Monterrey','Central America','Eastern Time (US & Canada)','Indiana (East)','Bogota','Lima','Quito','Atlantic Time (Canada)','Caracas','La Paz','Santiago','Newfoundland','Brasilia','Buenos Aires','Montevideo','Georgetown','Greenland','Mid-Atlantic','Azores','Cape Verde Is.','Dublin','Edinburgh','Lisbon','London','Casablanca','Monrovia','UTC','Belgrade','Bratislava','Budapest','Ljubljana','Prague','Sarajevo','Skopje','Warsaw','Zagreb','Brussels','Copenhagen','Madrid','Paris','Amsterdam','Berlin','Bern','Rome','Stockholm','Vienna','West Central Africa','Bucharest','Cairo','Helsinki','Kyiv','Riga','Sofia','Tallinn','Vilnius','Athens','Istanbul','Minsk','Jerusalem','Harare','Pretoria','Kaliningrad','Moscow','St. Petersburg','Volgograd','Samara','Kuwait','Riyadh','Nairobi','Baghdad','Tehran','Abu Dhabi','Muscat','Baku','Tbilisi','Yerevan','Kabul','Ekaterinburg','Islamabad','Karachi','Tashkent','Chennai','Kolkata','Mumbai','New Delhi','Kathmandu','Astana','Dhaka','Sri Jayawardenepura','Almaty','Novosibirsk','Rangoon','Bangkok','Hanoi','Jakarta','Krasnoyarsk','Beijing','Chongqing','Hong Kong','Urumqi','Kuala Lumpur','Singapore','Taipei','Perth','Irkutsk','Ulaanbaatar','Seoul','Osaka','Sapporo','Tokyo','Yakutsk','Darwin','Adelaide','Canberra','Melbourne','Sydney','Brisbane','Hobart','Vladivostok','Guam','Port Moresby','Magadan','Srednekolymsk','Solomon Is.','New Caledonia','Fiji','Kamchatka','Marshall Is.','Auckland','Wellington',"Nuku'alofa",'Tokelau Is.','Chatham Is.','Samoa','Europe/London','America/New_York','EST']
